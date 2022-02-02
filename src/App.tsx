d2a2b2b54e743d49d216f5f6c050b75c31719ed4
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-02 14:17:14 +0000

    can update the stae on chnage and detect the enter button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
+import axios from 'axios'
 import { Container } from '@mui/material'
+import { useState, KeyboardEvent, ChangeEvent } from 'react'
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 import Navbar from "./components/Navbar"
 import Searchbar from "./components/Searchbar"
 
 export default function App() {
+
+  const baseURL = 'https://striveschool-api.herokuapp.com/api/deezer'
+
+  const [query, setQuery] = useState('')
+  // const [searchResults, searchResults] = useState()
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)
+
+  const handleSubmit = (e: KeyboardEvent<HTMLDivElement>) => {
+      if (e.code === 'Enter') {
+        alert(query)
+      }
+  }
+
+  const axiosRequest = async (query: string) => {
+    try {
+       const response = await axios({ baseURL, url: query })
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
   return (
     <Router>
     <Navbar />
     <Container maxWidth="xl" style={{ marginTop: '1.5rem' }}>
       <Routes>
-        <Route path="/" element={ <Searchbar /> } />
+        <Route path="/" element={ <Searchbar query={query} handleChange={handleChange} handleSubmit={handleSubmit} /> } />
       </Routes>
     </Container>
     </Router>
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,8 +1,22 @@
 import { TextField } from '@mui/material'
-import { useState } from 'react'
+import { ChangeEvent, KeyboardEvent } from 'react'
 
-export default function Searchbar() {
+interface Props {
+    query: string
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void
+    handleSubmit: (e: KeyboardEvent<HTMLDivElement>) => void
+}
+
+export default function Searchbar({ query, handleChange, handleSubmit }: Props) {
+
     return (
-        <TextField label="Search" variant="outlined" fullWidth />
+        <TextField
+            label="Search"
+            variant="outlined" 
+            fullWidth
+            value={query}
+            onChange={handleChange}
+            onKeyUp={handleSubmit}
+        />
     )
 }
