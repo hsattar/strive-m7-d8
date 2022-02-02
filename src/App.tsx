0b518d47d9c73f5e5f0b39a77749eaccbefbeffa
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-02 13:42:10 +0000

    added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-function App() {
+import { Container } from '@mui/material'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import Navbar from "./components/Navbar"
+import Searchbar from "./components/Searchbar"
+
+export default function App() {
   return (
-    <div>hello</div>
+    <Router>
+    <Navbar />
+    <Container maxWidth="xl" style={{ marginTop: '1.5rem' }}>
+      <Routes>
+        <Route path="/" element={ <Searchbar /> } />
+      </Routes>
+    </Container>
+    </Router>
   )
 }
-
-export default App
---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,8 @@
+import { TextField } from '@mui/material'
+import { useState } from 'react'
+
+export default function Searchbar() {
+    return (
+        <TextField label="Search" variant="outlined" fullWidth />
+    )
+}
